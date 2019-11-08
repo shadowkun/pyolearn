ac835607906fa7b95d0bfeff1d1e8d152622eeb6
zhenyankun.zyk <zhenyankun.zyk@alibaba-inc.com>
2019-11-08 17:05:02 +0800

    pyo3 update

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
-extern crate cpython;
+use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
 
-use cpython::{PyDict, PyResult, Python};
-
-fn main() {
+fn main() -> Result<(), ()> {
     let gil = Python::acquire_gil();
-    hello(gil.python()).unwrap();
+    let py = gil.python();
+    main_(py).map_err(|e| {
+        // We can't display python error type via ::std::fmt::Display,
+        // so print error here manually.
+        e.print_and_set_sys_last_vars(py);
+    })
 }
 
-fn hello(py: Python) -> PyResult<()> {
+fn main_(py: Python) -> PyResult<()> {
     let sys = py.import("sys")?;
-    let version: String = sys.get(py, "version")?.extract(py)?;
-
-    let locals = PyDict::new(py);
-    locals.set_item(py, "os", py.import("os")?)?;
-    let user: String = py
-        .eval(
-            "os.getenv('USER') or os.getenv('USERNAME')",
-            None,
-            Some(&locals),
-        )?
-        .extract(py)?;
-
+    let version: String = sys.get("version")?.extract()?;
+    let locals = [("os", py.import("os")?)].into_py_dict(py);
+    let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
+    let user: String = py.eval(code, None, Some(&locals))?.extract()?;
     println!("Hello {}, I'm Python {}", user, version);
     Ok(())
 }
